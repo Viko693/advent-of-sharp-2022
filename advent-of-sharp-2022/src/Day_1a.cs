2052088a2f94e1024654be0fa887c19b299afce4
Viktor Djambov <vikogamedev@gmail.com>
2023-10-19 23:00:38 +0100

    Adjustments
    Testing

--- advent-of-sharp-2022/src/Day_1a.cs
+++ advent-of-sharp-2022/src/Day_1a.cs
@@ -1,27 +1,22 @@
 using System;
 using System.IO;
 
-class Question_1_Solution
-{
-    static void Main()
-    {
+class Question_1_Solution {
+    static void Main() {
         // Read all lines from the input.txt file into an array
         string[] lines = File.ReadAllLines("inputs/Day_1.txt");
 
-        int maxCalories = 0; // To store the maximum calories
-        int maxCaloriesElf = 0; // To store the elf number with maximum calories
-        int currentElfCalories = 0; // To store the current elf's total calories
-        int elfCounter = 1; // To keep track of the elf number
+        int maxCalories = 0; //  store the maximum calories
+        int maxCaloriesElf = 0; //  store the elf number with maximum calories
+        int currentElfCalories = 0; //  store the current elf's total calories
+        int elfCounter = 1; //  keep track of the elf number
 
         // Loop through each line in the input
-        for (int i = 0; i < lines.Length; i++)
-        {
+        for (int i = 0; i < lines.Length; i++) {
             // Check if the line is empty (new elf's data starts)
-            if (string.IsNullOrEmpty(lines[i]))
-            {
+            if (string.IsNullOrEmpty(lines[i])) {
                 // Check if the current elf has more calories than the max so far
-                if (currentElfCalories > maxCalories)
-                {
+                if (currentElfCalories > maxCalories) {
                     maxCalories = currentElfCalories;
                     maxCaloriesElf = elfCounter;
                 }
@@ -32,11 +27,15 @@ class Question_1_Solution
             }
 
             // Parse the calories from the line and add to the current elf's total
-            int calories = int.Parse(lines[i]);
-            currentElfCalories += calories;
+            try {
+                int calories = int.Parse(lines[i]);
+                currentElfCalories += calories;
+            } catch (FormatException e) {
+                Console.WriteLine("Error parsing line: " + lines[i]);
+            }
         }
 
-        // Output the elf with the maximum calories
+        // Output 
         Console.WriteLine("Elf " + maxCaloriesElf + " has the most calories: " + maxCalories);
     }
 }
